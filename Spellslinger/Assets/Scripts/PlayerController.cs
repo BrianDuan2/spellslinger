1a6f432471ade28df101dfd9d225b04d071e756d
BrianDuan2 <brianduanbns@gmail.com>
2020-10-05 22:57:22 -0700

    adding empty project

---
+++ Spellslinger/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0;
+    public float jumpForce;
+
+    public float jumps = 2;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        /*if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumps > 0)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
+            jumps--;
+        }
+    }
+}
