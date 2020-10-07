21b2d9425ce16d883e3e9b1cc1d68098993ffe15
BrianDuan2 <brianduanbns@gmail.com>
2020-10-07 00:47:43 -0700

    sample changes

---
+++ Spellslinger/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+     public PlayerController player;
+    private float playerJumps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            //player.jumps = 2;
+            player.jumps = 2;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+       // player.jumps--;
+       player.jumps--;
+    }
+}
