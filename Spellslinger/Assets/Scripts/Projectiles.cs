31e837b4a935572e0ca8d86c6c42ab8753901c54
BrianDuan2 <brianduanbns@gmail.com>
2020-10-10 20:44:43 -0700

    basic projectiles and enemies

---
+++ Spellslinger/Assets/Scripts/Projectiles.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("hit the ground");
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("hit an enemy");
+            Destroy(gameObject);
+        }
+    }
+}
