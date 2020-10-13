4aac0123f0cfae3bf383f5efa40a891d8f333b23
Dawood <dshafqat@sfu.ca>
2020-10-12 23:58:41 -0700

    Updated Enemy script to fix error

--- Spellslinger/Assets/Scripts/Enemy.cs
+++ Spellslinger/Assets/Scripts/Enemy.cs
@@ -6,7 +6,14 @@ public class Enemy : MonoBehaviour
 {
     public int health;
     public int maxHealth;
-    public float speed = 1f;
+    //public float speed = 1f;
+
+    public float moveSpeed = 3f;
+    Transform leftwayPoint, rightwayPoint;
+    Vector3 localScale;
+    bool movingRight = true;
+    Rigidbody2D rb;
+
     public float leftMax, rightMax;
     float walkingDirection = 1.0f;
     float originalX;
@@ -14,25 +21,62 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
-        originalX = transform.position.x;
+       // originalX = transform.position.x;
+       localScale = transform.localScale;
+       rb = GetComponent<Rigidbody2D>();
+       leftwayPoint = GameObject.Find("LeftWayPoint").GetComponent<Transform> ();
+       rightwayPoint = GameObject.Find("RightWayPoint").GetComponent<Transform> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (walkingDirection >0 && transform.position.x > originalX + rightMax)
-        {
-            walkingDirection *= -1;
-            transform.localScale = new Vector3(transform.localScale.x *= -1, transform.localScale.y, transform.localScale.z);
+      //  if (walkingDirection >0 && transform.position.x > originalX + rightMax)
+      //  {
+        //    walkingDirection *= -1;
+         //   transform.localScale = new Vector3(transform.localScale.x *= -1, transform.localScale.y, transform.localScale.z);
 
-        }
-        if(walkingDirection < 0 && transform.position.x < originalX - leftMax)
-        {
-            walkingDirection *= -1;
-            transform.localScale = new Vector3(transform.localScale.x *= -1, transform.localScale.y, transform.localScale.z);
-        }
-        transform.Translate(new Vector2(speed, 0) * walkingDirection * Time.deltaTime);
+      //  }
+      //  if(walkingDirection < 0 && transform.position.x < originalX - leftMax)
+       // {
+        //    walkingDirection *= -1;
+         //   transform.localScale = new Vector3(transform.localScale.x *= -1, transform.localScale.y, transform.localScale.z);
+        //}
+        //transform.Translate(new Vector2(speed, 0) * walkingDirection * Time.deltaTime);
+
+
+        if (transform.position.x > rightwayPoint.position.x)
+            movingRight = false;
+        if (transform.position.x < leftwayPoint.position.x)
+            movingRight = true;
+
+        if (movingRight)
+            moveRight();
+        else
+            moveLeft();
     }
+
+    void moveRight()
+    {
+        movingRight = true;
+        localScale.x = 1;
+        transform.localScale = localScale;
+        rb.velocity = new Vector2 (localScale.x * moveSpeed, rb.velocity.y);
+
+    }
+
+    void moveLeft()
+    {
+        movingRight = false;
+        localScale.x = -1;
+        transform.localScale = localScale;
+        rb.velocity = new Vector2 (localScale.x * moveSpeed, rb.velocity.y);
+
+    }
+
+
+
+
     public void TakeDamage (int damage)
     {
         health -= damage;
