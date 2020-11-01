87c8ea272a1c67c3b90fb9902dcc030772b56365
BrianDuan2 <brianduanbns@gmail.com>
2020-10-31 19:17:21 -0700

    added flying enemy
    Will shoot projectiles at the player
    Need to add aggro range and aiming

---
+++ Spellslinger/Assets/Scripts/Flyer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flyer : Enemy
+{   
+    //Enemy will fire projectiles
+    public GameObject shootPrefab;
+    //used for targetting the player
+    public PlayerController player;
+    //used to control fire rate
+    private float fireRate = 0;
+    // Start is called before the first frame update
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
