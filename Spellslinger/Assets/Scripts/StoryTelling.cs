8d6c8fc29693057e45f50ee9635932a736a11087
kimmychu <66520175+kimmychu@users.noreply.github.com>
2020-12-02 14:49:14 +0800

    Add placeholder for opening scene

--- Spellslinger/Assets/Scripts/StoryTelling.cs
+++ Spellslinger/Assets/Scripts/StoryTelling.cs
@@ -8,34 +8,50 @@ public class StoryTelling : MonoBehaviour
 {
     public Text dialogueText;
     private Queue<string> sentences;
+    private Queue<Sprite> cutscenes;
     public GameObject YesButton;
     public GameObject NoButton;
     public GameObject ContinueButton;
+    public GameObject CutSceneBox;
 
     public Animator animator;
 
     void Start()
     {
         sentences = new Queue<string>();
+        cutscenes = new Queue<Sprite>();
     }
 
-    public void StartDialogue (Dialogue dialogue)
+    public void StartDialogue(Dialogue dialogue)
     {
 
         animator.SetBool("IsOpen", true);
-
         sentences.Clear();
+        cutscenes.Clear();
 
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
+        foreach (Sprite cutscene in dialogue.cutscenes)
+        {
+            cutscenes.Enqueue(cutscene);
+        }
 
         DisplayNextSentence();
     }
 
-    public void DisplayNextSentence ()
+    public void DisplayNextSentence()
     {
+        if (cutscenes.Count == 0)
+        {
+            CutSceneBox.gameObject.SetActive(false);
+        }
+        else
+        {
+            Sprite cutscene = cutscenes.Dequeue();
+            CutSceneBox.GetComponent<Image>().sprite = cutscene;
+        }
         if (sentences.Count == 0)
         {
             EndDialogue();
@@ -44,9 +60,10 @@ public class StoryTelling : MonoBehaviour
         string sentence = sentences.Dequeue();
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
+
     }
 
-    IEnumerator TypeSentence (string sentence)
+    IEnumerator TypeSentence(string sentence)
     {
         dialogueText.text = "";
         foreach (char letter in sentence.ToCharArray())
